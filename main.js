f99b150c4583662e7f75b72054608638dbceb500
Sébastien renault <sre@zenexity.com>
2012-12-19 00:00:01 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+window.setTimeout(function() {
+    var object1 = document.getElementById('object1'),
+        object2 = document.getElementById('object2'),
+        object3 = document.getElementById('object3'),
+        object4 = document.getElementById('object4'),
+        object5 = document.getElementById('object5');
+
+    object1.style.opacity=0;
+    object2.className += ' transform';
+    object3.className += ' transform-origin';
+    object4.className += ' rotate';
+    object5.className += ' animation';
+}, 1000);
